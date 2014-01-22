74d9a324be7b20828a31bd366b8f6c8f4ae920cc
ReWiG <kirundel963@gmail.com>
2014-01-22 22:44:46 +0400

    ver 0.1

---
+++ Horse Creator/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Horse_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обновить программу";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Добавить картинку в базу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(512, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Отобразить картинку из базы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(464, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 354);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Horse Creator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Horse Creator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Data.SQLite;
+
+namespace Horse_Creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            long totalBytes;
+            byte[] buffer;
+            using (FileStream stream = new FileStream("2r.png", FileMode.Open, FileAccess.Read))
+            {
+	            BinaryReader br = new BinaryReader(stream);
+
+                totalBytes = new FileInfo("2r.png").Length;
+	            buffer = br.ReadBytes((Int32)totalBytes);
+	            stream.Close();
+	            br.Close();
+            }
+
+            SQLiteConnection conn = new SQLiteConnection(@"Data Source=horse.db");
+
+            //Подготовим запрос к базе на сохранение
+            SQLiteCommand sqlCommand = new SQLiteCommand();
+            sqlCommand.Connection = conn;
+            sqlCommand.CommandType = CommandType.Text;
+ 
+            //Текст запроса
+            sqlCommand.CommandText = "INSERT INTO Horse (horseName, horseImage) VALUES ('Название2', @Kartinka);";
+ 
+            //Параметр @Kartinka - наш массив байтов
+            sqlCommand.Parameters.Add("@Kartinka", DbType.Binary, buffer.Length).Value = buffer;
+ 
+            //Открываем соединение с базой данных
+            conn.Open();
+ 
+            //Выполняем запрос на запись
+            //и получаем уникальный идентификатор картинки
+            int intID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+ 
+            //Завершаем выполнение команды
+            sqlCommand.Cancel();
+ 
+            //Закрываем соединение с базой данных
+            conn.Close();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Подготовим соединение с базой данных
+            SQLiteConnection sqlConnection = new SQLiteConnection(@"Data Source=horse.db");
+
+            //Подготовим запрос к базе на сохранение
+            SQLiteCommand sqlCommand = new SQLiteCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandType = CommandType.Text;
+
+            //Текст запроса
+            sqlCommand.CommandText = "SELECT horseImage FROM horse WHERE id = @imageID";
+
+            //Параметр @imageID - наш идентификатор
+            sqlCommand.Parameters.Add("@imageID", DbType.Int32).Value = 1;
+
+            //Открываем соединение с базой данных
+            sqlConnection.Open();
+
+            //Выполняем запрос на чтение
+            byte[] ImageBytes = (byte[])sqlCommand.ExecuteScalar();
+
+            //Завершаем выполнение команды
+            sqlCommand.Cancel();
+
+            //Закрываем соединение с базой данных
+            sqlConnection.Close();
+
+            MemoryStream ImageStream = new MemoryStream(ImageBytes);
+
+            //Создадим изображение из потока
+            Image Image = Image.FromStream(ImageStream);
+
+            //Закроем поток
+            ImageStream.Close();
+
+            pictureBox1.Image = Image;
+        }
+    }
+}
